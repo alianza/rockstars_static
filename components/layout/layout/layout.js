96410818e0c6cc90b5aa5d6414fd3bebf5cf973f
Alianza <janwillemvanbremen@live.nl>
2021-11-08 22:51:35 +0100

    Update viewport meta tag

--- components/layout/layout/layout.js
+++ components/layout/layout/layout.js
@@ -35,7 +35,7 @@ export default function Layout({ children }) {
             <meta name="msapplication-TileColor" content="#da532c"/>
             <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
             <meta name="theme-color" content="#ffffff"/>
-            <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' />
+            <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover' />
             <link rel="manifest" href="/manifest.json"/>
             <link href="https://fonts.googleapis.com/css2?family=Abel&display=swap" rel="stylesheet"/>
         </Head>
