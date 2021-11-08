0c666783daf7b5a9393b2f3b06f9c2893cec1fb8
Alianza <janwillemvanbremen@live.nl>
2021-11-08 19:08:19 +0100

    Added Abel font via <head> tag in layout.js

--- components/layout/layout/layout.js
+++ components/layout/layout/layout.js
@@ -36,6 +36,7 @@ export default function Layout({ children }) {
             <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
             <meta name="theme-color" content="#ffffff"/>
             <link rel="manifest" href="/manifest.json"/>
+            <link href="https://fonts.googleapis.com/css2?family=Abel&display=swap" rel="stylesheet"/>
         </Head>
 
         <Header onMenuClick={toggleMenu} title={'Rockstars IT!'}/>
