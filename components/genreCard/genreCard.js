8c9e2137babee6ed9d15cf7715def723daed7055
Alianza <janwillemvanbremen@live.nl>
2021-11-09 05:26:20 +0100

    Remove duplicate id's

--- components/genreCard/genreCard.js
+++ components/genreCard/genreCard.js
@@ -5,7 +5,7 @@ import encodeURICustom from "../../lib/encodeURLs"
 export default function GenreCard(props) {
     return (
         <Link href={`/genre/${encodeURICustom(props.genre)}`}>
-            <a id="genreCard" className={`button text-secondary ${props.hidden ? 'hidden' : ''}`}>
+            <a className={`button text-secondary ${props.hidden ? 'hidden' : ''}`}>
                 {props.genre}
             </a>
         </Link>
--- components/songCard/songCard.js
+++ components/songCard/songCard.js
@@ -20,20 +20,20 @@ export default function SongCard(props) {
     const spotifyUrl = `https://open.spotify.com/track/${props.song.spotifyId}`
 
     return (
-        <div id="songCard" className={`relative bg-secondary p-4 w-full desktop:w-[calc(50%-.25em)] ${props.hidden ? 'hidden' : ''}`}>
-            <span className={'text-primary block mobile:mr-20'}>Title: <b>{props.song.name}</b></span>
-            <span className={'text-primary block'}>Album: <b>{props.song.album}</b></span>
+        <div className={`relative bg-secondary p-4 w-full desktop:w-[calc(50%-.25em)] ${props.hidden ? 'hidden' : ''}`}>
+            <span className="text-primary block mobile:mr-20">Title: <b>{props.song.name}</b></span>
+            <span className="text-primary block">Album: <b>{props.song.album}</b></span>
             {props.showArtist && <span className={'text-primary block'}>Artist: <Link href={`/artist/${encodeURICustom(props.song.artist)}`}><a><b>{props.song.artist}</b></a></Link></span> }
             {props.showGenre && <span className={'text-primary block'}>Genre: <Link href={`/genre/${encodeURICustom(props.song.genre)}`}><a><b>{props.song.genre}</b></a></Link></span> }
-            <span className={'text-primary block mr-20 mobile:mr-0'}>Year: <b>{props.song.year}</b></span>
-            <a className={'absolute w-[100px] bottom-0 right-0 mobile:top-0 mobile:bottom-auto '} href={spotifyUrl} target={'_blank'} rel={'noreferrer'}>
+            <span className="text-primary block mr-20 mobile:mr-0">Year: <b>{props.song.year}</b></span>
+            <a className="absolute w-[100px] bottom-0 right-0 mobile:top-0 mobile:bottom-auto" href={spotifyUrl} target="_blank" rel="noreferrer">
                 <Image
-                    layout={'responsive'}
+                    layout="responsive"
                     width={100}
                     height={41}
                     src="/assets/spotify.png"
-                    alt={'Listen on Spotify!'}
-                    className={'transition-transform hover:scale-105'}
+                    alt="Listen on Spotify!"
+                    className="transition-transform hover:scale-105"
                 />
             </a>
         </div>
