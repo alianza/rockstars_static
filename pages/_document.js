efa6970587cbcc0c465a1730f821d052f93d7f4c
Alianza <janwillemvanbremen@live.nl>
2021-11-09 03:56:51 +0100

    layout.js

--- pages/_document.js
+++ pages/_document.js
@@ -3,21 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function MyDocument() {
     return (
         <Html lang="en">
-            <Head>
-                <title>Team Rockstars IT - Tech Case</title>
-                <link rel="icon" href="/favicon.ico"/>
-                <meta name="viewport" content="width=device-width, initial-scale=1"/>
-                <meta name="description" content="Team Rockstars IT Tech Case statically rendered with Next.js"/>
-                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-                <meta name="msapplication-TileColor" content="#da532c"/>
-                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
-                <meta name="theme-color" content="#ffffff"/>
-                <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover' />
-                <link rel="manifest" href="/manifest.json"/>
-                <link href="https://fonts.googleapis.com/css2?family=Abel&display=swap" rel="stylesheet"/>
-            </Head>
+            <Head />
             <body>
             <Main/>
             <NextScript/>
