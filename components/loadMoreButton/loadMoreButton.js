5318c3049ef07c76180c69197d21db2c360ee0c3
Alianza <janwillemvanbremen@live.nl>
2021-11-09 05:29:12 +0100

    Fix loadMoreButton.js selectors and font size

--- components/loadMoreButton/loadMoreButton.js
+++ components/loadMoreButton/loadMoreButton.js
@@ -1,8 +1,9 @@
 export default function LoadMoreButton(props) {
     return (
         <button className={`button yellow shadow-3xl ${props.fullWidth ? 'w-full' : ''}`}
-                onClick={() => document.querySelectorAll('#artistCard.hidden, #songCard.hidden').forEach((e, i) => i < 50 ? e.classList.remove('hidden') : '')}>
-            <b>Load more...</b>
+                onClick={() => document.querySelectorAll('#artists a.hidden, #songs div.hidden, #genres a.hidden')
+                    .forEach((e, i) => i < 50 ? e.classList.remove('hidden') : '')}>
+            <strong className="text-xl">Load more...</strong>
         </button>
     )
 }
