7c215c32530a5a7be20bd9ced4b61084a52c5025
Alianza <janwillemvanbremen@live.nl>
2021-11-09 19:09:27 +0100

    Restore encodeURICustom

--- components/artistCard/artistCard.js
+++ components/artistCard/artistCard.js
@@ -1,9 +1,10 @@
 import React from "react"
 import Link from "next/link"
+import encodeURICustom from "../../lib/encodeURLs"
 
 export default function ArtistCard(props) {
     return (
-        <Link href={`/artist/${encodeURIComponent(props.artist.name)}`}>
+        <Link href={`/artist/${encodeURICustom(props.artist.name)}`}>
             <a className={`button text-secondary${props.hidden ? ' hidden' : ''}`}>
                 {props.artist.name}
             </a>
