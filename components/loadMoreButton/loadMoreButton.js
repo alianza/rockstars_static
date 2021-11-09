55363f16f08017548cf5dde6f082f5bf96b64571
Alianza <janwillemvanbremen@live.nl>
2021-11-09 19:06:57 +0100

    Fix loadMoreButton.js

--- components/loadMoreButton/loadMoreButton.js
+++ components/loadMoreButton/loadMoreButton.js
@@ -8,10 +8,17 @@ LoadMoreButton.propTypes = {
 export default function LoadMoreButton(props) {
     const selectors = '#artists a.hidden, #songs div.hidden, #genres a.hidden, #genre div.hidden'
 
+    const loadMore = () => {
+        document.querySelectorAll(selectors).forEach((e, i) => {
+            i < (props?.amount || 50) ? e.classList.remove('hidden') : ''
+        })
+
+        if (!document.querySelectorAll(selectors).length) { document.querySelector('#loadMore').remove() }
+    }
+
     return (
-        <button className={`button yellow shadow-3xl ${props.fullWidth ? 'w-full' : ''}`}
-                onClick={() => document.querySelectorAll(selectors)
-                    .forEach((e, i) => i < (props?.amount || 50) ? e.classList.remove('hidden') : '')}>
+        <button id="loadMore" className={`button yellow shadow-3xl ${props.fullWidth ? 'w-full' : ''}`}
+                onClick={() => loadMore()}>
             <strong className="text-xl">Load more...</strong>
         </button>
     )
