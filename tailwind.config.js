279ec25e7da9fe493dbece94bbaf965d76b951ac
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2022-02-08 19:11:57 +0100

    Updated Tailwindcss to v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
@@ -48,11 +46,6 @@ module.exports = {
         'header': 'var(--header)',
         'footer': 'var(--footer)',
       }
-    },
-  },
-  variants: {
-    extend: {
-      width: ['last'],
     },
   },
   plugins: [],
